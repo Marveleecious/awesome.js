3bee8f0cf828a5513a4bbce383d44e1a00abaf18
Marveleecious <richardtimilehin05@gmail.com>
2022-09-27 18:21:43 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+let score = 56;
+if(score >= 70 && score <= 100){
+    console.log('A')
+}else if(score >= 60 && score <= 69 ){
+  console.log('B');
+}else if(score >= 50 && score <= 59){
+  console.log('C');
+}else if(score >= 45 && score <= 49){
+    console.log('D');
+}else if(score >= 40 && score <= 44){
+    console.log('E');
+}else if(score >= 0 && score <= 39){
+    console.log('F');
+}
+
+let mark = 83;
+if(mark >= 50 && mark <= 100){
+  console.log('pass');
+}else if(mark >= 0 && mark <= 49){
+  console.log('fail');
+}
